ccee10e23e22174a624bdc71aec7f7d418b184ab
Alexey_Nadenenko1 <alexey_nadenenko1@epam.com>
2024-10-24 18:04:13 +0400

    comment ps1 script to fix build pipeline

--- Application.Test/Configurations/UpdateConfig.ps1
+++ Application.Test/Configurations/UpdateConfig.ps1
@@ -1,9 +1,9 @@
-$jsonPath = ".\Configurations\ReportPortalConfiguration.json"
-$config = Get-Content $jsonPath | ConvertFrom-Json
-$newJsonPath = ".\ReportPortal.json"
-
-# Modify the JSON content
-$config.server.apiKey = $env:API_KEY
-
-# Save the updated JSON
-$config | ConvertTo-Json -Depth 10 | Set-Content $newJsonPath
+#$jsonPath = ".\Configurations\ReportPortalConfiguration.json"
+#$config = Get-Content $jsonPath | ConvertFrom-Json
+#$newJsonPath = ".\ReportPortal.json"
+#
+## Modify the JSON content
+#$config.server.apiKey = $env:API_KEY
+#
+## Save the updated JSON
+#$config | ConvertTo-Json -Depth 10 | Set-Content $newJsonPath
